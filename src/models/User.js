d065843b0244d53d09cbd82dca0c942560526304
Renato Loureiro <renato92.hora@gmail.com>
2023-03-06 19:56:32 -0300

    dsada

---
+++ src/models/User.js
@@ -0,0 +1,14 @@
+const sequelize = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define ('User', {
+        id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+        display_name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        image: DataTypes.STRING,
+})
+
+}
+
+// fdsfsfdsf
