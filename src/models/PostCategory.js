e5eebe48b3a7cd844fd83e146f4911d073ed5a55
Renato Loureiro <renato92.hora@gmail.com>
2023-03-12 18:47:56 -0300

    Req 11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,35 @@
+const sequelize = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+    const PostCategory = sequelize.define('PostCategory', {
+        postId: {
+            type: DataTypes.INTEGER,
+            foreignKey: true,
+        },
+        categoryId: {
+            type: DataTypes.INTEGER,
+            foreignKey: true,
+        },
+    },
+    {
+        timestamps: false,
+        tableName: 'posts_category',
+        underscored: true,
+    })
+
+    PostCategory.associate = (models) => {
+        models.BlogPost.belongsToMany(models.Category, {
+            as: 'categories',
+            through: PostCategory,
+            foreignKey: 'postId',
+            otherKey: 'categoryId'
+        });
+        models.Category.belongsToMany(models.BlogPost, {
+            as: 'blog_posts',
+            through: PostCategory,
+            foreignKey: 'categoryId',
+            otherKey:'postId'
+        })
+    }
+    return PostCategory
+}
